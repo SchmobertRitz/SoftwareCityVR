0784bafa10a00af578ba45d778d345ed889459ce
stoppelkopp <stoppelwoppel@gmail.com>
2016-07-21 22:08:55 +0200

    Initial

---
+++ Simple Tree Map Test/Assets/Demo.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Demo : MonoBehaviour {
+    private ProjectManagerScript projectManager;
+     
+
+    public void Start()
+    {
+        projectManager = Component.FindObjectOfType<ProjectManagerScript>();
+
+        Dictionary<BuildingProperty, MetricMapping> mappings = new Dictionary<BuildingProperty, MetricMapping>();
+
+        List<string> metricNames = projectManager.currentProject.metricNames;
+
+        mappings.Add(BuildingProperty.Height, new MetricMapping(metricNames[7], BuildingProperty.Height, new ValueConverter(0, 1, 0, 1, false), null));
+        mappings.Add(BuildingProperty.Width, new MetricMapping(metricNames[10], BuildingProperty.Width, new ValueConverter(0, 1, 0, 1, false), null));
+        mappings.Add(BuildingProperty.Red, new MetricMapping(metricNames[0], BuildingProperty.Red, new ValueConverter(0, 100, 0, 1, true), null));
+        mappings.Add(BuildingProperty.Green, new MetricMapping(metricNames[0], BuildingProperty.Green, new ValueConverter(0, 100, 0, 1, true), null));
+        mappings.Add(BuildingProperty.Blue, new MetricMapping(metricNames[0], BuildingProperty.Blue, new ValueConverter(0, 100, 0, 1, true), null));
+
+        projectManager.OnMappingChanged(mappings);
+    }
+
+}
---
+++ Simple Tree Map Test/Assets/Grow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+
+public class Grow : MonoBehaviour {
+
+    public SliderControl sliderControl;
+    public float minScale = 1;
+    public float maxScale = 100;
+    
+    void Start()
+    {
+        transform.localScale = Vector3.one * minScale;
+        sliderControl.OnValueChanged += SetValue;
+    }
+
+    private void SetValue(float value)
+    {
+        transform.localScale = Vector3.one * (minScale + (maxScale - minScale) * value);
+    }
+}
